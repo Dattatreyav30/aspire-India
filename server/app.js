05212297bc07464f9a7e6615f883c629fb537d31
dattatreyav30 <dattatreya361@gmail.com>
2023-11-02 14:51:42 +0530

    made separate file and added all the relationships there

--- server/app.js
+++ server/app.js
@@ -7,46 +7,15 @@ const cors = require("cors");
 const app = express();
 
 const sequelize = require("./util/database");
+const relationships = require("./util/relationships");
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//models
-const User = require("./models/user/userModel");
-const tinyHabits = require("./models/user/tinyHabitsModel");
-const tinyHabitsCompletion = require("./models/user/tinyHabitCompletionModel");
-const personalityQuestions = require("./models/user/personalityQuestionModel");
-const personalityQnRecModel = require("./models/user/personalityQnRecModel");
-const personalityOutcomes = require("./models/user/personalityOutcomeModel");
-const personalityResults = require("./models/user/personalityResultsModel");
-
 // RELATIONSHIPS
-
-//user and tiny habits
-User.hasMany(tinyHabitsCompletion);
-tinyHabitsCompletion.belongsTo(User);
-
-tinyHabits.hasMany(tinyHabitsCompletion);
-tinyHabitsCompletion.belongsTo(tinyHabits);
-
-//user and personality
-
-User.hasMany(personalityQnRecModel);
-personalityQnRecModel.belongsTo(User);
-
-personalityQuestions.hasMany(personalityQnRecModel);
-personalityQnRecModel.belongsTo(personalityQuestions);
-
-
-//user and personality results
-User.hasMany(personalityResults)
-personalityResults.belongsTo(User)
-
-personalityOutcomes.hasMany(personalityResults);
-personalityResults.belongsTo(personalityOutcomes);
-
+relationships();
 sequelize
   .sync()
   .then(() => {
---
+++ server/util/relationships.js
@@ -0,0 +1,34 @@
+const User = require("../models/user/userModel");
+const tinyHabits = require("../models/user/tinyHabitsModel");
+const tinyHabitsCompletion = require("../models/user/tinyHabitCompletionModel");
+const personalityQuestions = require("../models/user/personalityQuestionModel");
+const personalityQnRecModel = require("../models/user/personalityQnRecModel");
+const personalityOutcomes = require("../models/user/personalityOutcomeModel");
+const personalityResults = require("../models/user/personalityResultsModel");
+
+const relationships = () => {
+  //user and tiny habits , many tiny habits can be completed by single user
+  User.hasMany(tinyHabitsCompletion);
+  tinyHabitsCompletion.belongsTo(User);
+
+  //here adding tinyhabitsquestion id so that we can relate which user is complted which habit
+  tinyHabits.hasMany(tinyHabitsCompletion);
+  tinyHabitsCompletion.belongsTo(tinyHabits);
+
+  //here linking userid so that each time user answered qn can be redcorded here
+  User.hasMany(personalityQnRecModel);
+  personalityQnRecModel.belongsTo(User);
+
+  // here linking with record model , so that which question is answered by user
+  personalityQuestions.hasMany(personalityQnRecModel);
+  personalityQnRecModel.belongsTo(personalityQuestions);
+
+  //ading userid and personalityOutcomesId. here , we can relate personality outcomes with id
+  User.hasMany(personalityResults);
+  personalityResults.belongsTo(User);
+
+  personalityOutcomes.hasMany(personalityResults);
+  personalityResults.belongsTo(personalityOutcomes);
+};
+
+module.exports = relationships;
